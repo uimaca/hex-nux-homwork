223c1159b4baa780b6986bdfd53ad4cc0a114bef
Maca <uimaca@gmail.com>
2024-12-13 23:03:10 +0800

    引入SCSS

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+    <NuxtLink to="/login">登入</NuxtLink>
+    <NuxtPage></NuxtPage>
+</div>    
+</template>
+
+<script setup>
+import '~/assets/all.scss'
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,14 @@
 <template>
   <div>
     <h1>首頁</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
   </div>
 </template>
 
